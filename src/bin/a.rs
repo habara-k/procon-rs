2ac2eb7776b45cc0fe333cc732e6710faaf335fe
Keigo Habara <k.habara.aa@gmail.com>
2022-02-20 00:10:03 +0900

    Update

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use itertools::Itertools;
 use num::complex::Complex;
+use num::integer::gcd;
 use ordered_float::OrderedFloat;
 use proconio::{
     fastout, input,
@@ -8,6 +9,7 @@ use proconio::{
 };
 use std::cmp::Reverse;
 use std::collections::{BTreeMap, BinaryHeap};
+use std::f64::consts::PI;
 use superslice::Ext;
 
 use ac_library_rs::*;

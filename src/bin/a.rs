f07970d0aad01b600c9d5a49f0693e3812cc7548
Keigo Habara <k.habara.aa@gmail.com>
2022-02-21 00:59:54 +0900

    Add std::cmp::{max,min}

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -7,8 +7,8 @@ use proconio::{
     fastout, input,
     marker::{Chars, Usize1},
 };
-use std::cmp::Reverse;
-use std::collections::{BTreeMap, BinaryHeap};
+use std::cmp::{max, min};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet};
 use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::Ext;
@@ -16,42 +16,7 @@ use superslice::Ext;
 use ac_library_rs::*;
 use my_library_rs::*;
 
-//#[fastout]
+#[fastout]
 fn main() {
-    input! {
-        h: usize, w: usize,
-        c: [Chars; h],
-    }
-
-    let disjoint = (0..1<<w).filter(|&s| {
-        (s & (s << 1)) == 0
-    }).collect_vec();
-
-    type Mint = ModInt1000000007;
-    let mut dp = vec![Mint::new(1); 1 << w];
-
-    for k in 0..h {
-
-        let mut wall = 0;
-        for i in 0..w {
-            if c[k][i] == '#' {
-                wall |= 1<<i;
-            }
-        }
-
-        let mut nxt_dp = vec![Mint::new(0); 1<<w];
-
-        for &s in disjoint.iter().filter(|&s| {
-            *s & wall == 0
-        }) {
-            nxt_dp[s] = dp[(1<<w)-1 & !(s | s>>1 | s<<1)];
-        }
-
-        // zeta
-        subset_zeta_transform(&mut nxt_dp);
-
-        swap(&mut dp, &mut nxt_dp);
-    }
-
-    println!("{}", dp[(1<<w)-1].val());
+    input! {}
 }

c88dc58d57e8090d7b6a38629695276a1d1c18f9
Keigo Habara <k.habara.aa@gmail.com>
2022-02-21 23:17:16 +0900

    Add text_io

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -8,10 +8,11 @@ use proconio::{
     marker::{Chars, Usize1},
 };
 use std::cmp::{max, min};
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet};
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
 use std::f64::consts::PI;
 use std::mem::swap;
 use superslice::Ext;
+use text_io::read;
 
 use ac_library_rs::*;
 use my_library_rs::*;

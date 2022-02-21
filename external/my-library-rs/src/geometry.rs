7645c43aa46df567681eb39ea8729af630ec2bd4
Keigo Habara <k.habara.aa@gmail.com>
2022-02-21 18:36:08 +0900

    Add cargo test

--- external/my-library-rs/src/geometry.rs
+++ external/my-library-rs/src/geometry.rs
@@ -1,7 +1,15 @@
-use itertools::Itertools;
 use num::complex::Complex;
 use num::{Num, Zero};
+use std::iter::Sum;
 
+/// 外積を返す.
+/// # Example
+///
+/// ```
+/// use num::Complex;
+/// use my_library_rs::*;
+/// assert_eq!(cross(Complex::new(4, 1), Complex::new(3, 2)), 4*2 - 1*3);
+/// ```
 pub fn cross<T>(a: Complex<T>, b: Complex<T>) -> T
 where
     T: Num,
@@ -9,41 +17,71 @@ where
     a.re * b.im - a.im * b.re
 }
 
-pub fn convex_hull<T>(ps: &mut Vec<Complex<T>>) -> Vec<Complex<T>>
+/// 反時計回りの凸包を返す.
+/// NOTE: (x座標,y座標)のtupleが一番小さい点を始点とする.
+/// # Example
+///
+/// ```
+/// use num::Complex;
+/// use my_library_rs::*;
+/// let ps = vec![Complex::new(0, 0), Complex::new(10, 0), Complex::new(0, 10), Complex::new(1, 1)];
+/// assert_eq!(convex_hull(&ps), vec![Complex::new(0, 0), Complex::new(10, 0), Complex::new(0, 10)]);
+/// ```
+pub fn convex_hull<T>(ps: &Vec<Complex<T>>) -> Vec<Complex<T>>
 where
     T: Copy + Num + Ord,
 {
-    ps.sort_by_key(|p| (p.re, p.im));
+    if ps.len() <= 1 {
+        return ps.clone();
+    }
+
+    let mut order: Vec<usize> = (0..ps.len()).collect();
+    order.sort_by_key(|&i| (ps[i].re, ps[i].im));
 
     let mut ch = vec![];
-    for &p in ps.iter() {
+    for &i in order.iter() {
         while ch.len() >= 2
-            && cross(p - ch[ch.len() - 1], ch[ch.len() - 1] - ch[ch.len() - 2]) >= Zero::zero()
+            && cross(
+                ps[i] - ch[ch.len() - 1],
+                ch[ch.len() - 1] - ch[ch.len() - 2],
+            ) >= Zero::zero()
         {
             ch.pop();
         }
-        ch.push(p);
+        ch.push(ps[i]);
     }
     let n = ch.len();
-    for &p in ps.iter().rev().skip(1) {
+    for &i in order.iter().rev().skip(1) {
         while ch.len() > n
-            && cross(p - ch[ch.len() - 1], ch[ch.len() - 1] - ch[ch.len() - 2]) >= Zero::zero()
+            && cross(
+                ps[i] - ch[ch.len() - 1],
+                ch[ch.len() - 1] - ch[ch.len() - 2],
+            ) >= Zero::zero()
         {
             ch.pop();
         }
-        ch.push(p);
+        ch.push(ps[i]);
     }
 
     ch.pop();
     ch
 }
+
+/// 多角形の符号付き面積の2倍を返す.
+/// # Example
+/// ```
+/// use num::Complex;
+/// use my_library_rs::*;
+/// let ps = vec![Complex::new(0, 0), Complex::new(1, 0), Complex::new(0, 1)];
+/// assert_eq!(area_x2(&ps), 1);
+///
+/// let ps = vec![Complex::new(0, 0), Complex::new(0, 1), Complex::new(1, 0)];
+/// assert_eq!(area_x2(&ps), -1);
+/// ```
 pub fn area_x2<T>(ps: &Vec<Complex<T>>) -> T
 where
-    T: Copy + Num,
+    T: Copy + Num + Sum,
 {
-    ps.iter()
-        .skip(1)
-        .tuple_windows()
-        .map(|(a, b)| cross(a - ps[0], b - ps[0]))
-        .fold(Zero::zero(), |sum, x| sum + x)
+    let n = ps.len();
+    (0..n).map(|i| cross(ps[i], ps[(i + 1) % n])).sum::<T>()
 }
--- external/my-library-rs/src/math.rs
+++ external/my-library-rs/src/math.rs
@@ -1,5 +1,5 @@
 use std::convert::From;
-use std::ops::{Div, Mul};
+use std::ops::{AddAssign, Div, Mul};
 
 pub struct Combination<T> {
     fact: Vec<T>,
@@ -53,7 +53,24 @@ fn bitwise_transform<T>(a: &mut Vec<T>, f: fn(*mut T, *mut T)) {
     }
 }
 
-use std::ops::AddAssign;
+/// 下位集合に対する高速ゼータ変換を行う.
+/// # Example
+/// ```
+/// use my_library_rs::*;
+/// let mut a = vec![0; 8];
+/// a[0b001] = 1;
+/// a[0b010] = 10;
+/// a[0b100] = 100;
+/// subset_zeta_transform(&mut a);
+/// assert_eq!(a[0b000], 0);
+/// assert_eq!(a[0b001], 1);
+/// assert_eq!(a[0b010], 10);
+/// assert_eq!(a[0b011], 11);
+/// assert_eq!(a[0b100], 100);
+/// assert_eq!(a[0b101], 101);
+/// assert_eq!(a[0b110], 110);
+/// assert_eq!(a[0b111], 111);
+/// ```
 pub fn subset_zeta_transform<T>(a: &mut Vec<T>)
 where
     T: Copy + AddAssign,

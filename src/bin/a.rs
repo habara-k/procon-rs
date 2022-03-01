6e3b0c7cb62f399e0b866b37139682d34b798ace
Keigo Habara <k.habara.aa@gmail.com>
2022-03-01 21:50:57 +0900

    Refacor impl Clone for Base

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -15,10 +15,81 @@ use std::mem::swap;
 use superslice::Ext;
 use text_io::read;
 
-use ac_library_rs::*;
-use my_library_rs::*;
+use my_library_rs::{MapMonoid, Monoid, PersistentRBLazySegtree, RangeApply, RangeFold, Tree};
+
+struct RangeSum;
+impl Monoid for RangeSum {
+    type S = (i64, usize);
+
+    fn identity() -> Self::S {
+        (0, 0)
+    }
+
+    fn binary_operation(&(a, n): &Self::S, &(b, m): &Self::S) -> Self::S {
+        (a + b, n + m)
+    }
+}
+struct RangeAddRangeSum;
+impl MapMonoid for RangeAddRangeSum {
+    type M = RangeSum;
+    type F = i64;
+
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(&a: &Self::F, &(x, n): &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
+        (a * n as i64 + x, n)
+    }
+
+    fn composition(&a: &Self::F, &b: &Self::F) -> Self::F {
+        a + b
+    }
+}
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize, q: usize,
+        x: [i64; n],
+    }
+
+    let mut seg: PersistentRBLazySegtree<RangeAddRangeSum> =
+        x.iter().map(|&v| (v, 1usize)).collect_vec().into();
+
+    let mut ans = vec![];
+    for _ in 0..q {
+        input! { t: usize }
+        match t {
+            1 => {
+                input! { a: Usize1, b: Usize1, v: i64 }
+                seg.apply_range(a, b + 1, v);
+            }
+            2 => {
+                input! { a: Usize1, b: Usize1, c: Usize1, d: Usize1 }
+                let x = seg.clone();
+                let (_, mut x, _) = x.split_range(c, d + 1);
+                // x = [c,d+1)
+
+                let y = seg.clone();
+                let (mut y, _, mut z) = y.split_range(a, b + 1);
+                // y = [0,a)
+                // z = [b+1,n)
+
+                y.merge(&mut x);
+                y.merge(&mut z);
+                swap(&mut y, &mut seg);
+                // seg = [0,a) + [c,d+1) + [b+1,n)
+            }
+            3 => {
+                input! { a: Usize1, b: Usize1 }
+                ans.push(seg.prod(a, b + 1).0);
+            }
+            _ => panic!(),
+        }
+    }
+
+    for e in ans {
+        println!("{}", e);
+    }
 }

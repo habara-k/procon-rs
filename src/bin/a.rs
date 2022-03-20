5bf18b23a065e7058af943e3a29113771b467983
Keigo Habara <k.habara.aa@gmail.com>
2022-03-20 21:31:38 +0900

    speedup butterfly

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -16,75 +16,33 @@ use superslice::Ext;
 use text_io::read;
 
 use my_library_rs::*;
-
-struct RangeSum;
-impl Monoid for RangeSum {
-    type S = (i64, usize);
-    fn identity() -> Self::S {
-        (0, 0)
-    }
-    fn binary_operation(&(a, n): &Self::S, &(b, m): &Self::S) -> Self::S {
-        (a + b, n + m)
-    }
-}
-struct RangeAddRangeSum;
-impl MapMonoid for RangeAddRangeSum {
-    type M = RangeSum;
-    type F = i64;
-    fn identity_map() -> Self::F {
-        0
-    }
-    fn mapping(&a: &Self::F, &(x, n): &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
-        (a * n as i64 + x, n)
-    }
-    fn composition(&a: &Self::F, &b: &Self::F) -> Self::F {
-        a + b
-    }
-}
+type Fps = Fps998244353;
 
 #[fastout]
 fn main() {
     input! {
-        n: usize, q: usize,
-        x: [i64; n],
+        n: u64, k: usize,
     }
 
-    let mut seg: RedBlackTree<RangeAddRangeSum> =
-        x.iter().map(|&v| (v, 1usize)).collect_vec().into();
-
-    let mut ans = vec![];
-    for _ in 0..q {
-        input! { t: usize }
-        match t {
-            1 => {
-                input! { a: Usize1, b: Usize1, v: i64 }
-                seg.apply_range(a, b + 1, v);
-            }
-            2 => {
-                input! { a: Usize1, b: Usize1, c: Usize1, d: Usize1 }
-                let x = seg.clone();
-                let (_, mut x, _) = x.split3(c, d + 1);
-                // x = [c,d+1)
-
-                let y = seg.clone();
-                let (mut y, _, mut z) = y.split3(a, b + 1);
-                // y = [0,a)
-                // z = [b+1,n)
-
-                y.merge(&mut x);
-                y.merge(&mut z);
-                swap(&mut y, &mut seg);
-                // seg = [0,a) + [c,d+1) + [b+1,n)
-            }
-            3 => {
-                input! { a: Usize1, b: Usize1 }
-                ans.push(seg.prod(a, b + 1).0);
-            }
-            _ => panic!(),
-        }
+    let mut mp = BTreeMap::new();
+    for i in 1..=k {
+        *mp.entry(k / i).or_insert(0) += 1;
     }
 
-    for e in ans {
-        println!("{}", e);
+    let mut fps: Fps = vec![1].into();
+    const MOD: u32 = 998244353;
+    for (sz, cnt) in mp {
+        let cnt = cnt % MOD;
+        fps *= if cnt == 0 { 0 } else { MOD - cnt };
+        fps.insert(0, 1);
+
+        fps = fps.inv(sz+2);
+        fps.remove(0);
+        fps /= cnt;
     }
+
+    fps *= MOD - 1;
+    fps.insert(0, 1);
+    let ans = Fps::bostan_mori(&vec![1], &fps, n+1);
+    println!("{}", ans);
 }
